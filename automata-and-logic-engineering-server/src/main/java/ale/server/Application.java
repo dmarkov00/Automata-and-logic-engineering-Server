8bc7455e845136a692a1cf1c2634e50322d2394f
Jack D <dmarkov23@gmail.com>
2018-02-20 15:04:03 +0100

    setup of project structure

---
+++ automata-and-logic-engineering-server/src/main/java/ale/server/Application.java
@@ -0,0 +1,14 @@
+package ale.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+// The starting point of the application
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
---
+++ automata-and-logic-engineering-server/src/main/java/ale/server/controllers/AssignmentController.java
@@ -0,0 +1,9 @@
+package ale.server.controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class AssignmentController {
+
+}
