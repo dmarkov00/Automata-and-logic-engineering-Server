b85f68c5d3bd6f3c8fb8f255ed4bc7cdc7420344
Jack D <dmarkov23@gmail.com>
2018-03-31 15:19:05 +0200

    setup tests with different formulas

--- automata-and-logic-engineering-server/src/test/java/TruthTableBuilderTest.java
+++ automata-and-logic-engineering-server/src/test/java/TruthTableBuilderTest.java
@@ -19,39 +19,61 @@ public class TruthTableBuilderTest {
 
     @Before
     public void setUp() {
+        Formula basicTestFormula = mock(Formula.class);
+        when(basicTestFormula.getFormula()).thenReturn(">(A,B)");
 
-        Formula testFormula = mock(Formula.class);
-        when(testFormula.getFormula()).thenReturn("=(>(A,B),|(~(A),C))");
 
-        AssignmentsCalculations assignmentsCalculations = new AssignmentsCalculations(testFormula);
-        assignmentTwoResult = assignmentsCalculations.generateAssignmentTwoResult();
+        Formula complexTestFormula = mock(Formula.class);
+        when(complexTestFormula.getFormula()).thenReturn("=(=( ~(>(~(A),B)), |( ~(A) ,B) ), &(C,=(~(Y),~(Z))))");
+
 
     }
 
     /**
-     * Test if the truth table values are correct.
+     * Test if the truth table values are correct with medium complex formula, this includes checks for:
      * 1. Number of table rows
-     * 2. The generated table values for each variable
-     * 3. The results when supplying the formula with the different values
+     * 2. The generated table binary values for each variable
+     * 3. The generated table result based on the supplied binary values
      */
     @Test
-    public void truthTableValuesGeneration() {
+    public void truthTableValuesGenerationWithMediumFormula() {
+
+        // Initialization
+        Formula mediumTestFormula = mock(Formula.class);
+        when(mediumTestFormula.getFormula()).thenReturn("=(>(A,B),|(~(A),C))");
+
+        AssignmentsCalculations assignmentsCalculations = new AssignmentsCalculations(mediumTestFormula);
+        assignmentTwoResult = assignmentsCalculations.generateAssignmentTwoResult();
+
         // Retrieve the data
         List<Map<Character, Integer>> tableResult = assignmentTwoResult.getTableResults();
 
         assertThat(tableResult.size(), is(8));
 
+        // Test for the first row of the table
         List<Integer> rowStub = new ArrayList<>(Arrays.asList(0, 0, 0, 0));
-
 
         int firstRowAValue = tableResult.get(0).get('A');
         int firstRowBValue = tableResult.get(0).get('B');
         int firstRowCValue = tableResult.get(0).get('C');
         int firstRowResult = tableResult.get(0).get('=');
+
         List<Integer> rowValues = new ArrayList<>(Arrays.asList(firstRowAValue, firstRowBValue, firstRowCValue, firstRowResult));
 
+        assertThat(rowValues, is(rowStub));
+
+        // Test for the fifth row of the table
+        rowStub = new ArrayList<>(Arrays.asList(1, 0, 1, 1));
+
+        int fifthRowAValue = tableResult.get(5).get('A');
+        int fifthRowBValue = tableResult.get(5).get('B');
+        int fifthRowCValue = tableResult.get(5).get('C');
+        int fifthRowResult = tableResult.get(5).get('=');
+
+        rowValues = new ArrayList<>(Arrays.asList(fifthRowAValue, fifthRowBValue, fifthRowCValue, fifthRowResult));
 
         assertThat(rowValues, is(rowStub));
+
 
     }
 
