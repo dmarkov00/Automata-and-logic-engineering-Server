85a0f67c7f6344cf07c9418d986d7f273585455d
Jack D <dmarkov23@gmail.com>
2018-04-02 11:47:27 +0200

    testing table data with a basic formula

--- automata-and-logic-engineering-server/src/test/java/TruthTableBuilderTest.java
+++ automata-and-logic-engineering-server/src/test/java/TruthTableBuilderTest.java
@@ -19,9 +19,6 @@ public class TruthTableBuilderTest {
 
     @Before
     public void setUp() {
-        Formula basicTestFormula = mock(Formula.class);
-        when(basicTestFormula.getFormula()).thenReturn(">(A,B)");
-
 
         Formula complexTestFormula = mock(Formula.class);
         when(complexTestFormula.getFormula()).thenReturn("=(=( ~(>(~(A),B)), |( ~(A) ,B) ), &(C,=(~(Y),~(Z))))");
@@ -30,10 +27,81 @@ public class TruthTableBuilderTest {
     }
 
     /**
-     * Test if the truth table values are correct with medium complex formula, this includes checks for:
+     * Test if the values representing the table(the data on top of the table) are correct with a medium complexity formula
+     * 1. The extracted formula variables are unique
+     * 2. The extracted formula variables are sorted alphabetically
+     * 3. The formula string is correct
+     */
+    @Test
+    public void tableDataGenerationWithBasicFormula() {
+
+        // Initialization
+        Formula basicTestFormula = mock(Formula.class);
+        when(basicTestFormula.getFormula()).thenReturn("&(A,B)");
+
+        AssignmentsCalculations assignmentsCalculations = new AssignmentsCalculations(basicTestFormula);
+        assignmentTwoResult = assignmentsCalculations.generateAssignmentTwoResult();
+
+        List<String> tableData = assignmentTwoResult.getTableData();
+
+        List<String> tableDataStub = new ArrayList<>(Arrays.asList("A", "B", "&(A,B)"));
+
+        assertThat(tableData, is(tableDataStub));
+
+    }
+
+
+    /**
+     * Test if the truth table values are correct with a basic complexity formula, this includes checks for:
      * 1. Number of table rows
-     * 2. The generated table binary values for each variable
-     * 3. The generated table result based on the supplied binary values
+     * 2. The generated table binary values for each variable, on certain "interesting" rows
+     * 3. The generated table result based on the supplied binary values, on certain "interesting" rows
+     */
+    @Test
+    public void truthTableValuesGenerationWithBasicFormula() {
+
+        // Initialization
+        Formula basicTestFormula = mock(Formula.class);
+        when(basicTestFormula.getFormula()).thenReturn("&(A,B)");
+
+        AssignmentsCalculations assignmentsCalculations = new AssignmentsCalculations(basicTestFormula);
+        assignmentTwoResult = assignmentsCalculations.generateAssignmentTwoResult();
+
+        // Retrieve the data
+        List<Map<Character, Integer>> tableResult = assignmentTwoResult.getTableResults();
+
+        // Check if the nr of table rows is correct
+        assertThat(tableResult.size(), is(4));
+
+        // Test for the first row of the table
+        List<Integer> rowStub = new ArrayList<>(Arrays.asList(0, 0));
+
+        int firstRowAValue = tableResult.get(0).get('A');
+        int firstRowBValue = tableResult.get(0).get('B');
+        int firstRowResult = tableResult.get(0).get('=');
+
+        List<Integer> rowValues = new ArrayList<>(Arrays.asList(firstRowAValue, firstRowBValue, firstRowResult));
+
+        assertThat(rowValues, is(rowStub));
+
+        // Test for the third row of the table
+        rowStub = new ArrayList<>(Arrays.asList(1, 1, 1));
+
+        int thirdRowAValue = tableResult.get(3).get('A');
+        int thirdRowBValue = tableResult.get(3).get('B');
+        int thirdRowResult = tableResult.get(3).get('=');
+
+        rowValues = new ArrayList<>(Arrays.asList(thirdRowAValue, thirdRowBValue, thirdRowResult));
+
+        assertThat(rowValues, is(rowStub));
+    }
+
+
+    /**
+     * Test if the truth table values are correct with a medium complexity formula, this includes checks for:
+     * 1. Number of table rows
+     * 2. The generated table binary values for each variable, on certain "interesting" rows
+     * 3. The generated table result based on the supplied binary values, on certain "interesting" rows
      */
     @Test
     public void truthTableValuesGenerationWithMediumFormula() {
@@ -48,6 +116,7 @@ public class TruthTableBuilderTest {
         // Retrieve the data
         List<Map<Character, Integer>> tableResult = assignmentTwoResult.getTableResults();
 
+        // Check if the nr of table rows is correct
         assertThat(tableResult.size(), is(8));
 
         // Test for the first row of the table
@@ -74,17 +143,17 @@ public class TruthTableBuilderTest {
 
         assertThat(rowValues, is(rowStub));
 
-
     }
 
     /**
-     * Test if the values representing the table(the data on top of the table) are correct
+     * Test if the values representing the table(the data on top of the table) are correct with a medium complexity formula
      * 1. The extracted formula variables are unique
      * 2. The extracted formula variables are sorted alphabetically
      * 3. The formula string is correct
      */
     @Test
     public void tableDataGeneration() {
+
 
     }
 
