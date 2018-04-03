30e0b64146cce18c8e3125797f50bda666856223
Jack D <dmarkov23@gmail.com>
2018-04-03 12:14:38 +0200

    finished up testing truth table generation

--- automata-and-logic-engineering-server/src/test/java/TruthTableBuilderTest.java
+++ automata-and-logic-engineering-server/src/test/java/TruthTableBuilderTest.java
@@ -1,7 +1,6 @@
 import ale.server.ale.calculations.AssignmentsCalculations;
 import ale.server.models.AssignmentTwoResult;
 import ale.server.models.Formula;
-import org.junit.Before;
 import org.junit.Test;
 
 import java.util.ArrayList;
@@ -17,14 +16,6 @@ import static org.mockito.Mockito.when;
 public class TruthTableBuilderTest {
     private AssignmentTwoResult assignmentTwoResult;
 
-    @Before
-    public void setUp() {
-
-        Formula complexTestFormula = mock(Formula.class);
-        when(complexTestFormula.getFormula()).thenReturn("=(=(~(>(~(A),B)),|(~(A),B)),&(C,=(~(Y),~(Z))))");
-
-
-    }
 
     /**
      * Test if the values representing the table(the data on top of the table) are correct, this includes:
@@ -182,6 +173,7 @@ public class TruthTableBuilderTest {
         assertThat(rowValues, is(rowStub));
 
     }
+
     /**
      * Test if the hash code is generated correctly
      */
@@ -197,6 +189,99 @@ public class TruthTableBuilderTest {
         String hashCode = assignmentTwoResult.getHashCode();
 
         assertThat(hashCode, is("9f"));
+    }
+
+    /**
+     * Test if the values representing the table(the data on top of the table) are correct
+     * 1. The extracted formula variables are unique
+     * 2. The extracted formula variables are sorted alphabetically
+     * 3. The formula string is correct
+     */
+    @Test
+    public void tableDataGenerationWithFormula3() {
+        // Initialization
+        Formula testFormula = mock(Formula.class);
+        when(testFormula.getFormula()).thenReturn("=(=(~(>(~(A),B)),|(~(A),B)),&(C,=(~(Y),~(Z))))");
+
+        AssignmentsCalculations assignmentsCalculations = new AssignmentsCalculations(testFormula);
+        assignmentTwoResult = assignmentsCalculations.generateAssignmentTwoResult();
+
+        List<String> tableData = assignmentTwoResult.getTableData();
+
+        List<String> tableDataStub = new ArrayList<>(Arrays.asList("A", "B", "C", "Y", "Z", "=(=(~(>(~(A),B)),|(~(A),B)),&(C,=(~(Y),~(Z))))"));
+
+        assertThat(tableData, is(tableDataStub));
+
+    }
+
+    /**
+     * Test if the truth table values are correct, this includes checks for:
+     * 1. Number of table rows
+     * 2. The generated table binary values for each variable, on certain "interesting" rows
+     * 3. The generated table result based on the supplied binary values, on certain "interesting" rows
+     */
+    @Test
+    public void truthTableValuesGenerationFormula3() {
+
+        // Initialization
+        Formula testFormula = mock(Formula.class);
+        when(testFormula.getFormula()).thenReturn("=(=(~(>(~(A),B)),|(~(A),B)),&(C,=(~(Y),~(Z))))");
+
+        AssignmentsCalculations assignmentsCalculations = new AssignmentsCalculations(testFormula);
+        assignmentTwoResult = assignmentsCalculations.generateAssignmentTwoResult();
+
+        // Retrieve the data
+        List<Map<Character, Integer>> tableResult = assignmentTwoResult.getTableResults();
+
+        // Check if the nr of table rows is correct
+        assertThat(tableResult.size(), is(32));
+
+        // Test for the first row of the table
+        List<Integer> rowStub = new ArrayList<>(Arrays.asList(0, 0, 0, 0, 0, 0));
+
+        int firstRowAValue = tableResult.get(0).get('A');
+        int firstRowBValue = tableResult.get(0).get('B');
+        int firstRowCValue = tableResult.get(0).get('C');
+        int firstRowYValue = tableResult.get(0).get('Y');
+        int firstRowZValue = tableResult.get(0).get('Z');
+
+        int firstRowResult = tableResult.get(0).get('=');
+
+        List<Integer> rowValues = new ArrayList<>(Arrays.asList(firstRowAValue, firstRowBValue, firstRowCValue, firstRowYValue, firstRowZValue, firstRowResult));
+
+        assertThat(rowValues, is(rowStub));
+
+        // Test for the sixth row of the table
+        rowStub = new ArrayList<>(Arrays.asList(0, 0, 1, 1, 0, 0));
+
+        int sixthRowAValue = tableResult.get(6).get('A');
+        int sixthRowBValue = tableResult.get(6).get('B');
+        int sixthRowCValue = tableResult.get(6).get('C');
+        int sixthRowYValue = tableResult.get(6).get('Y');
+        int sixthRowZValue = tableResult.get(6).get('Z');
+        int sixthRowResult = tableResult.get(6).get('=');
+
+        rowValues = new ArrayList<>(Arrays.asList(sixthRowAValue, sixthRowBValue, sixthRowCValue, sixthRowYValue, sixthRowZValue, sixthRowResult));
+
+        assertThat(rowValues, is(rowStub));
+
+    }
+
+    /**
+     * Test if the hash code is generated correctly
+     */
+    @Test
+    public void hashGenerationWithFormula3() {
+
+        // Initialization
+        Formula testFormula = mock(Formula.class);
+        when(testFormula.getFormula()).thenReturn("=(=(~(>(~(A),B)),|(~(A),B)),&(C,=(~(Y),~(Z))))");
+
+        AssignmentsCalculations assignmentsCalculations = new AssignmentsCalculations(testFormula);
+        assignmentTwoResult = assignmentsCalculations.generateAssignmentTwoResult();
+        String hashCode = assignmentTwoResult.getHashCode();
+
+        assertThat(hashCode, is("6f906f90"));
     }
 
 }
